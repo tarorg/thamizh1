f20d85673d08cbe5735f258d399babe2ece510e8
wetarorg <tarframework@gmail.com>
2024-09-17 23:23:00 +0530

    ta12

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import dotenv from 'dotenv';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // Load environment variables
 dotenv.config();
 
 export default defineConfig({
   output: 'server',  // Change this to 'server' for SSR
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: cloudflare(),
   vite: {
     define: {
       'import.meta.env.TURSO_DATABASE_URL': JSON.stringify(process.env.TURSO_DATABASE_URL),
